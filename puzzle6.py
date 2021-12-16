8f79c56207a4cb65fa1bc4c09a635e595158b6a4
aponbas <aponbas@users.noreply.github.com>
2021-12-17 00:54:45 +0100

    puzzle6b

--- puzzle6.py
+++ puzzle6.py
@@ -1,11 +1,23 @@
+import math
+
 with open("input6.txt", "r") as file:
     data = file.read().split(",")
 fishes = [int(i) for i in data]
 
-for day in range(0, 80):
-    for i in range(0, len(fishes)):
-        fishes[i] -= 1
-        if fishes[i] == -1:
-            fishes[i] = 6
-            fishes.append(8)
+# for day in range(0, 256):
+#     for i in range(0, len(fishes)):
+#         fishes[i] -= 1
+#         if fishes[i] == -1:
+#             fishes[i] = 6
+#             fishes.append(8)
+#     print(day)
+# print(len(fishes))
+
 print(len(fishes))
+max_days = 256
+new_borns = 0
+for day in range(0, max_days):
+    for fish in fishes:
+        new_borns += math.ceil((max_days-day-fish)/7)
+    print(new_borns)
+    break
