1229d18b8ac5b78dd6672da64f4458dbccdfd45e
aponbas <aponbas@users.noreply.github.com>
2021-12-26 21:28:09 +0100

    adding puzzle 5

--- puzzle5.py
+++ puzzle5.py
@@ -1,40 +1,65 @@
 with open("input5.txt", "r") as file:
     data = file.read().splitlines()
-import time
-vector = []
-grid = [[0]*1000]*1000
 
-for line in data:
-    coordinates = line.split(" -> ")
-    A = coordinates[0].split(",")
-    B = coordinates[1].split(",")
-    x_1 = int(A[0])
-    x_2 = int(B[0])
-    y_1 = int(A[1])
-    y_2 = int(B[1])
 
-    if x_1 == x_2:
-        vector = list(range(min(y_1, y_2), max(y_1, y_2)+1))
-        for i in vector:
-            grid[x_1][i] += 1
-            # print("hierx=x")
-            # print(i)
-            # print(x_1)
-            # print(grid[x_1][i])
-            # time.sleep(0.1)
-            # print(str(x_1) + "  " + str(i))
-    elif y_1 == y_2:
-        vector = list(range(min(x_1, x_2), max(x_1, x_2)+1))
-        for i in vector:
-            grid[y_1][i] += 1
-            # print("hiery=y")
-            # print(i)
-            # print(y_1)
-            # print(grid[i][y_1])
-            # time.sleep(0.1)
-overlap = 0
-for i in grid:
-    for j in i:
-        if j > 1:
-            overlap += 1
-print(overlap)
+def count_overlap(data, diagonal=False):
+    def get_coordinates(row):
+        coordinates = row.split(" -> ")
+        x_1, y_1 = coordinates[0].split(",")
+        x_2, y_2 = coordinates[1].split(",")
+
+        x_1 = int(x_1)
+        y_1 = int(y_1)
+        x_2 = int(x_2)
+        y_2 = int(y_2)
+        return x_1, y_1, x_2, y_2
+
+    def create_list(i, j):
+        if i > j:
+            li = list(range(i, j-1, -1))
+        else:
+            li = list(range(i, j+1))
+        return li
+
+    def count_grid(grid):
+        overlap = 0
+        for row in grid:
+            overlap += len(row)-row.count(0)-row.count(1)
+        return overlap
+
+    grid_size = 1000
+    grid = [[0] * grid_size for i in range(grid_size)]
+
+    for row in data:
+        x_1, y_1, x_2, y_2 = get_coordinates(row)
+
+        x_length = abs(x_1 - x_2) + 1
+        y_length = abs(y_1 - y_2) + 1
+
+        # If the line is a diagonal
+        if x_length == y_length:
+            # And if a diagonal should be added
+            if diagonal:
+                x_length = 1
+                y_length = 1
+            # Else skip adding the diagonal
+            else:
+                continue
+
+        x_vector = create_list(x_1, x_2) * y_length
+        y_vector = create_list(y_1, y_2) * x_length
+
+        for x, y in zip(x_vector, y_vector):
+            grid[y][x] += 1
+
+    overlap = count_grid(grid)
+    return overlap
+
+
+# Part 1
+
+print("puzzle 5a: " + str(count_overlap(data)))
+
+# Part 2
+
+print("puzzle 5b: " + str(count_overlap(data, True)))
